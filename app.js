bd76eb3af83db787432f0af96745b4304035125f
SebastienLouit <sebastien.louit@gmail.com>
2016-06-01 10:23:13 +0100

    Changing link to profile

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use('/api', expressJWT({ secret: secret })
   .unless({
     path: [
       { url: '/api/login', methods: ['POST'] },
-      { url: '/api/register', methods: ['POST'] }
+      { url: '/api/register', methods: ['POST'] },
+      { url: '/api/teams', methods: ['GET'] },
     ]
   }));
 
