1535ac36f629f974bf7b6d7323a4d57bf92b3176
SebastienLouit <sebastien.louit@gmail.com>
2016-06-02 18:51:31 +0100

    80 styling

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ var userSchema = mongoose.Schema({
   image:       { type: String },
   description: { type: String },
   competances: { type: String },
+  role:        { type: String },
   email:       { type: String, unique: true, required: true },
   password:    { type: String, required: true }
 }, {
